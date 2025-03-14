05907bb8eb890bd75a63c0e9ccb102d15774e731
tomtang <tomtang@aaxischina.com>
2025-03-14 17:59:07 +0800

    add loading for logout

--- components/business/Logout.tsx
+++ components/business/Logout.tsx
@@ -1,12 +1,27 @@
+"use client";
+
+import { useTransition } from "react";
 import { Icons } from "@components/ui/icon";
 import { Button } from "@components/ui/button";
 import { logoutAction } from "@lib/action";
+import { GlobalLoading } from "@components/business/globalLoading";
 
 export const Logout = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const handleLogout = () => {
+    startTransition(async () => {
+      await logoutAction();
+    });
+  };
+
   return (
-    <Button variant="ghost" onClick={logoutAction}>
-      <Icons.Power />
-      <span>退出登录</span>
-    </Button>
+    <>
+      {isPending && <GlobalLoading />}
+      <Button variant="ghost" onClick={handleLogout} disabled={isPending}>
+        <Icons.Power />
+        <span>退出登录</span>
+      </Button>
+    </>
   );
 };
