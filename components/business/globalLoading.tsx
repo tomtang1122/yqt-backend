17499abb1a1e36113a889da46be9e84fce071595
tomtang <tomtang@aaxischina.com>
2025-03-14 17:08:58 +0800

    update loading style

--- components/business/globalLoading.tsx
+++ components/business/globalLoading.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@components/ui/icon";
 
 export const GlobalLoading = () => {
   return (
-    <div className="flex flex-col gap-4 fixed inset-0 items-center justify-center bg-black/50 z-[1000]">
+    <div className="flex flex-col gap-4 fixed inset-0 items-center justify-center bg-gray-100 opacity-50 z-[1000]">
       <Icons.Loader className="w-10 h-10 animate-spin" />
       加载中...
     </div>
