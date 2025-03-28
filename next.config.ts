e8f39e56bfb1fdeb4ba9c5dde5e57f9481f6d56e
tomtang <tomtang@aaxischina.com>
2025-03-28 10:26:12 +0800

    add image configured Host

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   basePath: process.env.BASE_PATH,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yunquetai.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
